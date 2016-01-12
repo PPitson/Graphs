57091390d861fd1a4d5271d1314a51334702a649
Krzysztof Suruło <krzysztof.surulo@gmail.com>
2016-01-12 20:01:01 +0100

    Bug with cycle fixed

--- algorithms/Cycle.scala
+++ algorithms/Cycle.scala
@@ -5,22 +5,28 @@ import utils._
 object Cycle {
   def DFS(g: Graph,color:Array[Boolean],u: Int):Boolean={
     color(u)=true;
+    var result=false;
+    
     for(i <-g.graph(u)){
-      if(color(i.label)==false)
-        DFS(g,color,i.label);
-      if(color(i.label)==true)
-        true;
+       println(u+" "+i.label+" "+color(i.label),result);
+      if(color(i.label)==false && result==false)
+        result=DFS(g,color,i.label);
+      else(color(i.label)==true)
+        result=true;
     }
-    false;
+    result;
   }
   def isCyclic(g: Graph): Boolean = {
+    println("Start");
+    var result=false;
     val Supp=new Support;
     var color: Array[Boolean] = new Array[Boolean](g.vertices.size)
     for(j<-1 until g.vertices.size){
       if(DFS(g,color,j))
-        true;
+        result=true;
       Supp.clearColor(color);
+      
     }
-    false;
+    result;
   }
 }
